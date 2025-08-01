20547770e010826102ac67c53f0d6892194b6ace
Himanshu-sps <himanshu_sharma@softprodigy.com>
2025-08-01 11:03:04 +0530

    added .enc files

--- src/utils/AppConstants.ts
+++ src/utils/AppConstants.ts
@@ -1,10 +1,8 @@
 import { MoodType } from '@/type';
 
-export const OPENAI_API_KEY =
-  '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';
+//export const OPENAI_API_KEY =
+//  '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';
 export const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions';
-
-export const GEMINI_API_KEY = 'AIzaSyAMrP4N_LWGOOxBVerFy0yTijXDkVYvN0E';
 export const GEMINI_API_URL =
   'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent';
 
